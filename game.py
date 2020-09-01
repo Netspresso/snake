2b50c901ce48c6d31025b5e83a817751ed701be4
Dawid Marszalik <proconqpa@gmail.com>
2020-09-01 20:06:23 +0200

    zaczeta strona funkcjonalna, brakuje grafiki i mechanizmu zjadania

--- game.py
+++ game.py
@@ -16,23 +16,59 @@ class Game():
         self.apple = Apple()
         self.snake = Snake()
 
+    def drawGrid(self):
+        for x in range(0, Config.WINDOW_WIDTH, Config.CELLSIZE):
+            ''' Rysuje podział wertykalny '''
+            pygame.draw.line(self.screen, Config.DARKGRAY, (x, 0),
+                             (x, Config.WINDOW_HEIGHT))
+        for y in range(0, Config.WINDOW_HEIGHT, Config.CELLSIZE):
+            ''' Rysuje podział horyzontalny '''
+            pygame.draw.line(self.screen, Config.DARKGRAY, (0, y),
+                             (Config.WINDOW_WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(Config.BG_COLOR)
+        self.drawGrid()
+        pygame.display.update()
+        self.clock.tick(Config.FPS)
+
+    def handleKeyEvents(self, event):
+        if event.key == pygame.K_ESCAPE:
+            pygame.quit()
+
+    def resetGame(self):
+        del self.snake
+        del self.apple
+        self.snake = Snake()
+        self.apple = Apple()
+
+        return True
+
+    def isGameOver(self):
+        if (self.snake.snakeCoords[self.snake.HEAD]['x'] == -1
+                or self.snake.snakeCoords[self.snake.HEAD]['x']
+                == Config.CELLWIDTH
+                or self.snake.snakeCoords[self.snake.HEAD]['y'] == -1
+                or self.snake.snakeCoords[self.snake.HEAD]['y']
+                == Config.CELLWIDTH):
+            return self.resetGame()
+
+        for snakeBody in self.snake.snakeCoords[1:]:
+            if snakeBody['x'] == self.snake.snakeCoords[self.snake.HEAD][
+                    'x'] and snakeBody['y'] == self.snake.snakeCoords[
+                        self.snake.HEAD]['y']:
+                return self.resetGame()
+
     def run(self):
         while True:  # Main game loop
             for event in pygame.event.get(
             ):  # ta funkcja bierze i usuwa wiadomości z kolejki (można w argumencie funkcji zadać typ który jako jedyny ma być obsługiwany)
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        pygame.quit()
-                        sys.exit()
-            self.screen.fill(
-                (255, 255, 255)
-            )  # funkcją display utworzyłem obiekt Surface - fill to metoda dla obiektu Surface która wypełnia powierzchnię okna jednolitym kolorem
+                    self.handleKeyEvents(event)
 
-            pygame.display.update(
-            )  # metoda która odświeża wyświetlane okno - w mojej pętli będzie odświeżana za każdym zdarzeniem
-            self.clock.tick(
-                60
-            )  # ustala ilość cyknięć w milisekundach, w moim przypadku determinuje to że gra będzie działać w 60 klatkach na sekundę
+            self.snake.update(self.apple)
+            self.draw()
+            if self.isGameOver():
+                break
--- snake.py
+++ snake.py
@@ -13,7 +13,7 @@ class Snake():
         self.x = random.randint(5, Config.CELLWIDTH - 6)
         self.y = random.randint(5, Config.CELLHEIGHT - 6)
         self.direction = self.RIGHT
-        self.wprmCoords = [{
+        self.snakeCoords = [{
             'x': self.x,
             'y': self.y
         }, {
@@ -23,3 +23,34 @@ class Snake():
             'x': self.x - 2,
             'y': self.y
         }]
+
+    def update(self, apple):
+        ''' Sprawdza czy wonsz zjadl jablko'''
+        if self.snakeCoords[self.HEAD]['x'] == apple.x and self.snakeCoords[
+                self.HEAD]['y'] == apple.y:
+            apple.setNewLocation()
+        else:
+            del self.snakeCoords[-1]  # Usuwa segment ogona weza
+        '''Porusza wezem poprzez dodawanie segmentu na kierunku w ktorym waz sie porusza'''
+        if self.direction == self.UP:
+            newHead = {
+                'x': self.snakeCoords[self.HEAD]['x'],
+                'y': self.snakeCoords[self.HEAD]['y'] - 1
+            }
+        elif self.direction == self.DOWN:
+            newHead = {
+                'x': self.snakeCoords[self.HEAD]['x'],
+                'y': self.snakeCoords[self.HEAD]['y'] + 1
+            }
+        elif self.direction == self.LEFT:
+            newHead = {
+                'x': self.snakeCoords[self.HEAD]['x'] - 1,
+                'y': self.snakeCoords[self.HEAD]['y']
+            }
+        elif self.direction == self.RIGHT:
+            newHead = {
+                'x': self.snakeCoords[self.HEAD]['x'] + 1,
+                'y': self.snakeCoords[self.HEAD]['y']
+            }
+
+        self.snakeCoords.insert(0, newHead)
