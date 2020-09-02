69fa5c5a7c101286bf6a095fc6da235ed85317f8
Dawid Marszalik <proconqpa@gmail.com>
2020-09-02 10:13:32 +0200

    Wszystko dziala, dodam jeszcze wynik

--- game.py
+++ game.py
@@ -26,13 +26,45 @@ class Game():
             pygame.draw.line(self.screen, Config.DARKGRAY, (0, y),
                              (Config.WINDOW_WIDTH, y))
 
+    def drawSnake(self):
+        for coord in self.snake.snakeCoords:
+            x = coord['x'] * Config.CELLSIZE
+            y = coord['y'] * Config.CELLSIZE
+            snakeSegmentRect = pygame.Rect(x, y, Config.CELLSIZE,
+                                           Config.CELLSIZE)
+            pygame.draw.rect(self.screen, Config.DARKGREEN, snakeSegmentRect)
+            snakeInnerSegmentRect = pygame.Rect(x + 4, y + 4,
+                                                Config.CELLSIZE - 8,
+                                                Config.CELLSIZE - 8)
+            pygame.draw.rect(self.screen, Config.GREEN, snakeInnerSegmentRect)
+
+    def drawApple(self):
+        x = self.apple.x * Config.CELLSIZE
+        y = self.apple.y * Config.CELLSIZE
+        appleRect = pygame.Rect(x, y, Config.CELLSIZE, Config.CELLSIZE)
+        pygame.draw.rect(self.screen, Config.RED, appleRect)
+
     def draw(self):
         self.screen.fill(Config.BG_COLOR)
         self.drawGrid()
+        self.drawSnake()
+        self.drawApple()
         pygame.display.update()
         self.clock.tick(Config.FPS)
 
     def handleKeyEvents(self, event):
+        if (event.key == pygame.K_LEFT or event.key
+                == pygame.K_a) and self.snake.direction != self.snake.RIGHT:
+            self.snake.direction = self.snake.LEFT
+        elif (event.key == pygame.K_UP or event.key
+              == pygame.K_w) and self.snake.direction != self.snake.DOWN:
+            self.snake.direction = self.snake.UP
+        elif (event.key == pygame.K_RIGHT or event.key
+              == pygame.K_d) and self.snake.direction != self.snake.LEFT:
+            self.snake.direction = self.snake.RIGHT
+        elif (event.key == pygame.K_DOWN or event.key
+              == pygame.K_s) and self.snake.direction != self.snake.UP:
+            self.snake.direction = self.snake.DOWN
         if event.key == pygame.K_ESCAPE:
             pygame.quit()
 
